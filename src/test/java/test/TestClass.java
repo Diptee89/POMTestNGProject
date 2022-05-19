a2fa4f33342a2d3f5f5852160631d13be450d12d
DSingh <DSingh@INHYDLDSINGH2.logisticsdev.intra>
2022-05-19 11:38:18 +0530

    up

--- src/test/java/test/TestClass.java
+++ src/test/java/test/TestClass.java
@@ -2,11 +2,11 @@ package test;
 
 import java.time.Duration;
 import java.util.Iterator;
-import java.util.Random;
 import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -17,46 +17,9 @@ import pages.LoginPage;
 import pages.ManifestInformationPage;
 import pages.ManifestListPage;
 
-/**
- * Page Object encapsulates the Cargo Module
- */
-public class CargoTest {
+public class TestClass {
 	String driverPath = "c:\\Drivers\\IEDriverServer.exe";
 	protected WebDriver driver;
-
-	@Test(priority = 0)
-	public void testManifest() {
-		
-		ManifestListPage objMNFList = new ManifestListPage(driver);
-		ManifestInformationPage objMNFInfo = new ManifestInformationPage(driver);
-		HouseBillPage objHBL = new HouseBillPage(driver);
-		
-//		Create and Submit Manifest
-		login_ValidUser("nas.csa", "fx5test");
-		objMNFList.clickCargoMenu();
-		objMNFList.clickNew();
-		objMNFInfo.createManifest();
-		objHBL.createBL();
-		objMNFInfo.submitManifest();
-		logOut();
-		
-//		Approve Manifest
-		login_ValidUser("cmanifest.kwi", "fx5test");
-		objMNFList.clickCargoMenu();
-		objMNFList.searchWithTempNo(objMNFInfo.tempManifestNo);		//"TMRN/8655/KWI22"
-		objMNFList.clickTempNo();
-		objMNFInfo.approveManifest();
-		logOut();
-		
-//		Issue DO
-		login_ValidUser("nas.csa", "fx5test");
-		objMNFList.clickCargoMenu();
-		objMNFList.searchWithTempNo(objMNFInfo.tempManifestNo);	
-//		objMNFList.seachWithManifestNo(objMNFInfo.manifestNo);//   "MRN/7346/KWI22"
-		objMNFList.clickTempNo();
-		objMNFInfo.issueDOs();
-		logOut();
-	}
 
 	@BeforeTest
 	public void setUp() {
@@ -67,6 +30,45 @@ public class CargoTest {
 		driver.get("http://10.138.108.44/MCKWFX5TEST/Main.aspx");
 		System.out.println(driver.getTitle());
 		switchToWindow();
+	}
+
+	@Test
+	public void testManifest() {
+
+		ManifestListPage objMNFList = new ManifestListPage(driver);
+		ManifestInformationPage objMNFInfo = new ManifestInformationPage(driver);
+		HouseBillPage objHBL = new HouseBillPage(driver);
+
+//		Create and Submit Manifest
+		login("nas.csa", "fx5test");
+		objMNFList.clickCargoMenu();
+		objMNFList.clickNew();
+		objMNFInfo.createManifest();
+		objHBL.createBL();
+		objMNFInfo.submitManifest();
+		logOut();
+
+//		Approve Manifest
+		login("cmanifest.kwi", "fx5test");
+		objMNFList.clickCargoMenu();
+		objMNFList.searchWithTempNo(objMNFInfo.tempManifestNo); // "TMRN/8655/KWI22"
+		objMNFList.clickTempNo();
+		objMNFInfo.approveManifest();
+		logOut();
+
+//		Issue DO
+		login("nas.csa", "fx5test");
+		objMNFList.clickCargoMenu();
+		objMNFList.searchWithTempNo(objMNFInfo.tempManifestNo);
+//		objMNFList.seachWithManifestNo(objMNFInfo.manifestNo);//   "MRN/7346/KWI22"
+		objMNFList.clickTempNo();
+		objMNFInfo.issueDOs();
+		logOut();
+	}
+
+	@AfterTest
+	public void close() {
+		driver.close();
 	}
 
 	public void switchToWindow() {
@@ -87,25 +89,15 @@ public class CargoTest {
 		}
 	}
 
-//	@AfterTest
-	public void close() {
-		driver.close();
-	}
-	public void login_ValidUser(String id, String pass) {
-		// Create Login Page object
+	public void login(String id, String pass) {
+
 		LoginPage objLogin = new LoginPage(driver);
 		objLogin.loginValidUser(id, pass);
-		// Create Home Page Object
-//		HomePage objHome = new HomePage(driver);
-//		// Verify home page
-////		Assert.assertTrue(objHome.getLoggedInUserID().toUpperCase().contains("NAS.CSA"));
-//		System.out.println(objHome.getloggedInUserLabel() + ": " + objHome.getLoggedInUserID());
-//		System.out.println("Default port is " + objHome.getLoginPortName());
+
 	}
 
 	public void logOut() {
 		LogOutPage objLogOut = new LogOutPage(driver);
 		objLogOut.logOutUser();
 	}
-	
 }
